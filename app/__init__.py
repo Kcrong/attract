56475890e6786ce35ae9bcb95a7ad12a9bd26b03
kcrong <kcrong@kim82536.pe.kr>
2015-10-16 02:47:23 +0900

    fix models.py error

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,15 @@ import os
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
-app = Flask(__name__,template_folder=tmpl_dir)
+app = Flask(__name__, template_folder=tmpl_dir)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
 def create_app():
-	from sunrin_app import sunrin_app_blueprint
-	app.register_blueprint(sunrin_app_blueprint)
-	app.config.from_pyfile('../config.cfg')
-	return app
+    from sunrin_app import sunrin_app_blueprint
+    app.register_blueprint(sunrin_app_blueprint)
+    app.config.from_pyfile('../config.cfg')
+    return app
--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 # -*-coding: utf-8 -*-
 
-from app import create_app, manager, db, MigrateCommand
+from app import create_app, manager
 
 app = create_app()
+
 
 @manager.command
 def run():
     app.run(host='0.0.0.0')
+
 
 if __name__ == '__main__':
     manager.run()
