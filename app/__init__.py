6b06ccda7da55dba35ae87aa67b35406514c0c5f
nerobit <nero.union12@gmail.com>
2015-10-21 00:27:58 +0900

    add account app but need to repactoring. Very dirty

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,22 @@ import os
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
 app = Flask(__name__, template_folder=tmpl_dir)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
+db = SQLAlchemy()
 
 
 def create_app():
     from sunrin_app import sunrin_app_blueprint
+    from account import account_bp
+
     app.register_blueprint(sunrin_app_blueprint)
+    app.register_blueprint(account_bp, url_prefix='/account')
     app.config.from_pyfile('../config.cfg')
     return app
+
+app = create_app()
+db = SQLAlchemy(app)
+print dir(db)
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
