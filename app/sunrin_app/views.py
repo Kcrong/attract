956c86769795840d4676ec50fe44537aaca5fa7f
kcrong <kcrong@kim82536.pe.kr>
2015-10-29 19:36:57 +0900

    edit main index html

--- app/sunrin_app/views.py
+++ app/sunrin_app/views.py
@@ -1,8 +1,37 @@
 # -*-coding:utf-8-*-
 
 from . import sunrin_app_blueprint
-from flask import render_template
+from flask import render_template, send_from_directory, send_file
+
 
 @sunrin_app_blueprint.route('/')
+@sunrin_app_blueprint.route('/index')
 def index():
     return render_template('index.html')
+
+
+@sunrin_app_blueprint.route('/select')
+def select():
+    return render_template('select.html')
+
+
+# for send static files
+
+@sunrin_app_blueprint.route('/css/<path:filename>')
+def css_static(filename):
+    return send_from_directory(sunrin_app_blueprint.root_path + '/../static/css/', filename)
+
+
+@sunrin_app_blueprint.route('/js/<path:filename>')
+def js_static(filename):
+    return send_from_directory(sunrin_app_blueprint.root_path + '/../static/js/', filename)
+
+
+@sunrin_app_blueprint.route('/img/<path:filename>')
+def img_static(filename):
+    return send_from_directory(sunrin_app_blueprint.root_path + '/../static/images/', filename)
+
+
+@sunrin_app_blueprint.route('/favicon.ico')
+def favicon():
+    return send_file(sunrin_app_blueprint.root_path + '/../static/images/favicon.ico')
