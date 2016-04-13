56df16eecaaf2676848eea74dcdc91f3d3747a34
kcrong <kcrong@kim82536.pe.kr>
2016-04-13 21:23:01 +0900

    관리 파일에서 사용하지 않는 함수 삭제 Todo_list 파일 삭제

--- manage.py
+++ manage.py
@@ -1,26 +1,13 @@
 # -*-coding: utf-8 -*-
 
-from app import app, manager
-from flask import url_for
+from app import manager, create_app
+
+app = create_app()
 
 
 @manager.command
-def list_routes():
-    import urllib
-    output = []
-    for rule in app.url_map.iter_rules():
-
-        options = {}
-        for arg in rule.arguments:
-            options[arg] = "[{0}]".format(arg)
-
-        methods = ','.join(rule.methods)
-        url = url_for(rule.endpoint, **options)
-        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
-        output.append(line)
-
-    for line in sorted(output):
-        print line
+def run():
+    app.run()
 
 
 if __name__ == '__main__':
