297dac0a57ad2ee8614b36e646d4eef7951228a5
kcrong <kcrong@kim82536.pe.kr>
2016-04-14 00:43:22 +0900

    모델 __init__ 개선 각 테이블 별 컬럼 이름 변경 익명유저 클래스 생성

--- app/models.py
+++ app/models.py
@@ -1,49 +1,48 @@
 from . import db
-from flask.ext.login import UserMixin
+from flask.ext.login import UserMixin, AnonymousUserMixin
 
 
 class Party(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), unique=True)
-    promises = db.relationship('Promise', lazy='dynamic', backref='Party')
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    promises = db.relationship('Promise', lazy='dynamic', backref='party')
 
-    def __init__(self, title):
-        self.title = title
+    def __repr__(self):
+        return "<Party %s>" % self.name
 
-    def __unicode__(self):
-        return self.title
+    def __init__(self, name):
+        self.name = name
+
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    promise_id = db.Column(db.Integer, db.ForeignKey('promise.id'))
+    active = db.Column(db.BOOLEAN, default=True)
+    ip = db.Column(db.String(16), nullable=False)
+
+    def __repr__(self):
+        return "<Like %s>" % self.promise.title
+
+    def __init__(self, ip):
+        self.ip = ip
 
 
 class Promise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    party = db.Column(db.Integer, db.ForeignKey('party.id'))
+    party_id = db.Column(db.Integer, db.ForeignKey('party.id'))
     date = db.Column(db.DATE, nullable=False)
-    title = db.Column(db.String(30))
-    details = db.Column(db.String(300))
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(300))
     percentage = db.Column(db.Integer, nullable=False, default=0)
-    likes = db.relationship('Likes', lazy='dynamic', backref='Promise')
+    likes = db.relationship(Like, lazy='dynamic', backref='promise')
 
-    def __init__(self, title, details, party, date):
+    def __repr__(self):
+        return "<Promise %s>" % self.title
+
+    def __init__(self, title, detail, date):
         self.title = title
-        self.details = details
-        self.party = party
+        self.detail = detail
         self.date = date
-
-
-class Likes(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    promise = db.Column(db.Integer, db.ForeignKey('promise.id'))
-    like = db.Column(db.BOOLEAN)
-    ip = db.Column(db.String(16))
-
-    def __init__(self, promise, like, ip):
-        self.promise = promise
-        self.like = like
-        self.ip = ip
-
-    def __unicode__(self):
-        res = len(list(self.query.filter_by(id=self.id)))
-        return str(res)
 
 
 class Checklist(db.Model):
@@ -54,25 +53,19 @@ class Checklist(db.Model):
     info = db.Column(db.String(300))
     title = db.Column(db.String(300))
 
-    def __init__(self, page, picture, info, title, promise):
-        self.page = page
-        self.picture = picture
-        self.info = info
-        self.title = title
-        self.promise = promise
 
-
-class Users(db.Model, UserMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True, autoincrement=True)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
     userid = db.Column(db.String(30), unique=False)
-    fb_id = db.Column(db.String(30), unique=True)
     name = db.Column(db.String(10))
     password = db.Column(db.String(50))
-    active_yn = db.Column(db.BOOLEAN, nullable=False, default=True)
+    active = db.Column(db.BOOLEAN, nullable=False, default=True)
 
-    def __init__(self, userid=None, password=None, fb_id=None, name=None):
-        self.password = password
+    def __init__(self, userid, password, name):
         self.userid = userid
-        self.fb_id = fb_id
+        self.password = password
         self.name = name
+
+
+class GuestUser(AnonymousUserMixin):
+    name = 'Guest'
