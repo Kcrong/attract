03958fd6c80396a70f574f8df679c7c26379acaf
kcrong <kcrong@kim82536.pe.kr>
2016-04-13 21:23:48 +0900

    모델 파일 통합

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-# -*-coding: utf-8 -*-
-from .. import db
+from . import db
+from flask.ext.login import UserMixin
 
 
 class Party(db.Model):
@@ -12,6 +12,7 @@ class Party(db.Model):
 
     def __unicode__(self):
         return self.title
+
 
 class Promise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -44,6 +45,7 @@ class Likes(db.Model):
         res = len(list(self.query.filter_by(id=self.id)))
         return str(res)
 
+
 class Checklist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     promise = db.Column(db.Integer, db.ForeignKey('promise.id'))
@@ -58,3 +60,19 @@ class Checklist(db.Model):
         self.info = info
         self.title = title
         self.promise = promise
+
+
+class Users(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True, autoincrement=True)
+    userid = db.Column(db.String(30), unique=False)
+    fb_id = db.Column(db.String(30), unique=True)
+    name = db.Column(db.String(10))
+    password = db.Column(db.String(50))
+    active_yn = db.Column(db.BOOLEAN, nullable=False, default=True)
+
+    def __init__(self, userid=None, password=None, fb_id=None, name=None):
+        self.password = password
+        self.userid = userid
+        self.fb_id = fb_id
+        self.name = name
