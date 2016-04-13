f7b09288b5827c08e0bb1a6db7a2bde304705af0
kcrong <kcrong@kim82536.pe.kr>
2016-04-13 21:20:55 +0900

    blueprint 앱 이름 변경

--- app/DoMyung/__init__.py
+++ app/DoMyung/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding:utf-8 -*-
 from flask import Blueprint
-domyung_bp = Blueprint('/domyung', __name__)
+domyung_bp = Blueprint('domyung', __name__)
 from . import views
--- app/account/__init__.py
+++ app/account/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding:utf-8 -*-
 from flask import Blueprint
-account_bp = Blueprint('/account', __name__)
+account_bp = Blueprint('account', __name__)
 from . import views
